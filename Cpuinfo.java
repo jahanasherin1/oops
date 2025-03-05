5c3a6f5c552f33f1e07abe96374a6945839ae89b
jahanasherin1 <jahanasherin2311@gmail.com>
2025-03-05 15:18:14 +0530

    Completed

---
+++ Cpuinfo.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+
+class Cpu{
+    int price;
+      
+   class Processor{
+          String no_of_cores,manufacturer;   
+          
+          void display(){
+              System.out.println("Processor Manufacturer:" +manufacturer +"\n");
+              System.out.println("No of cores of processor:" +no_of_cores +"\n");
+              }
+              }
+          
+          static class Ram{
+             int memory;
+             String manufacturer;
+                
+                void display(){
+                     System.out.println("RAM Memory:" +memory +"\n");
+                     System.out.println("RAM Manufacturer:" +manufacturer+"\n");
+                     }
+                     }
+                     
+                void display(){
+                    System.out.println("Price:" + price);
+                    }
+                    }
+                    
+                   public class Cpuinfo{
+                       public static void main(String args[]){
+                       Cpu ncpu=new Cpu();
+                     
+                        
+                         Cpu.Processor processor=ncpu.new Processor();
+                         Cpu.Ram ram=new Cpu.Ram();
+                         Scanner sc=new Scanner(System.in);
+                            
+                            System.out.println("Enter the processor manufacturer:");
+                            processor.manufacturer=sc.next();
+                                          
+                            System.out.println("Enter the no of cores of processor:");
+                            processor.no_of_cores=sc.next();      
+                            
+                            System.out.println("Enter the RAM Memory size:");
+                            ram.memory=sc.nextInt(); 
+                            
+                            System.out.println("Enter the RAM Manufacturer:");
+                            ram.manufacturer=sc.next();
+                                         
+                            System.out.println("Enter the price:");
+                            ncpu.price=sc.nextInt();             
+                                         
+                            System.out.println("\nInformation of processor and RAM:\n");
+                            processor.display();
+                            ram.display();
+                            ncpu.display();
+                            }
+                            }
+                           
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                                         
+                           
+                           
